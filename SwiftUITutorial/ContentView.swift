a832c27a52638ebb5767c4788d7a0308d13366b5
Vishnu-at-196395329624 <Vishnu-at-196395329624>
2021-03-16 12:55:24 +0530

    Initial Commit

---
+++ SwiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial
+//
+//  Created by Vishnu IOS on 16/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
